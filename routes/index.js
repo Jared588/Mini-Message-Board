0495b559ea78acd69a112c36ba7f078a3a137ca2
Jared588 <jaredeprice@gmail.com>
2024-06-07 13:47:12 +0200

    Handle/display new posts

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini-Messageboard', messages: messages });
 });
 
+/* GET new message form */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Create message' });
+});
+
+/* Handle post requests */
+router.post('/new', function(req, res, next) {
+  // Get info from req.body
+  const { user, message } = req.body;
+  // Add info to array
+  messages.push({ text: message, user: user, added: new Date()})
+  // Redirect to /
+  res.redirect('/')
+});
+
 module.exports = router;
